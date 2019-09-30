5aad40c5104fdb4255a760592100c6d85f78f6bc
MelnikovAlerksei <melnikov.aleksei.alexeyevich@gmail.com>
2019-09-30 09:53:29 +0300

    created repo, logTriangle()

---
+++ exercises1/log-triangle/js/main.js
@@ -0,0 +1,16 @@
+
+  function logTriangleFor (heigth) {
+    let part = ""
+    for (let i = 0; i <= heigth; i++) {
+      part += "#"
+      console.log(part);
+    }
+  }
+  function logTriangleDoWhile (heigth) {
+    let part = "";
+    do {
+      part += "#";
+      console.log(part);
+    } while (part.length <= heigth)
+  }
+  logTriangleFor(7)
