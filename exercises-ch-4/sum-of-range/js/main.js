71c5fb4735131258ccfe6a79b0db9c0cb0037e1a
MelnikovAleksei <melnikov.aleksei.alexeyevich@gmail.com>
2019-10-04 22:46:03 +0300

    created full long version range func and sum func

--- exercises-ch-4/sum-of-range/js/main.js
+++ exercises-ch-4/sum-of-range/js/main.js
@@ -1,13 +1,31 @@
-function range(start, end) {
+function range(start, end, step) {
   let arr = [];
-  if (start > end) {
-    for (let i = start; i >= end; i--) {
+  if (step < 0) {
+      step *= -1;
+  }
+  if (step === undefined) {
+    for (let i = start; i <= end; i++) {
+        arr.push(i);
+    }
+  } else if (start > end) {
+    for (let i = start; i >= end; i -= step) {
       arr.push(i);
     }
   } else {
-      for (let i = start; i <= end; i++) {
+      for (let i = start; i <= end; i += step) {
         arr.push(i);
       }
     }
   return arr;
 }
+console.log(range(1, 10, 2))
+
+function sum (arr) {
+    let result = 0;
+    for (let value of arr) {
+        result += value;
+    }
+    return result;
+}
+
+console.log(sum(range(1, 10, 2)))
