bfb646b0f5908211edead1da740ec4c11ecdeb74
MelnikovAleksei <melnikov.aleksei.alexeyevich@gmail.com>
2019-10-07 10:00:46 +0300

    created 4 func
    arrayToList
    listToArray
    prepend
    nth func

--- exercises-ch-4/reversing-an-array/js/main.js
+++ exercises-ch-4/reversing-an-array/js/main.js
@@ -1,18 +1,9 @@
 function reverseArray (array) {
     let output = [];
-    
+
     for (let i = array.length - 1; i >= 0; i--) {
         output.push(array[i]);
     }
-    
-    return output;
-}
-let arr = [1, 2, 3];
-console.log(reverseArray(arr))
 
-function reverseArrayInPlace (array) {
-    for (let i = array.length - 1; i >= 0) {
-        array[i] = array.push
-    }
-    
+    return output;
 }
