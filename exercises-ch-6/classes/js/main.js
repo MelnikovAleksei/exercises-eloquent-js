7aca787f6a3c4651eae04870e481e8dde4cd3d1f
MelnikovAleksei <melnikov.aleksei.alexeyevich@gmail.com>
2020-01-08 13:58:57 +0300

    html js

---
+++ exercises-ch-6/classes/js/main.js
@@ -0,0 +1,14 @@
+class Rabbit {
+  constructor(type) {
+    this.type = type;
+  }
+  speak(line) {
+    console.log(`the ${this.type} rabbit says '${line}'`);
+  }
+}
+
+class box {
+  constructor(selector) {
+    
+  }
+}
